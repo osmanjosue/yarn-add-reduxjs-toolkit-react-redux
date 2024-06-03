b6a75a9f54737e107194b9cfb1573d12d748e982
osmanjosue <osmanjosue@gmail.com>
2024-06-03 12:12:40 -0600

    Seccion 18 => Clase: Tarea - decrement e incrementBy

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,24 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { increment } from './store/slices/counter/counterSlice';
+import { increment, decrement, incrementBy } from './store/slices/counter/counterSlice';
 
 function App() {
-  const {counter} = useSelector(state => state.counter)
-  const dispatch = useDispatch()
+  const { counter } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>count is {counter}</h1>
       <div className="card">
-        <button onClick={()=> dispatch(increment())}>
-          count is {counter}
-        </button>
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
+        <button onClick={() => dispatch(incrementBy(2))}>Increment by 2</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
--- src/store/slices/counter/counterSlice.js
+++ src/store/slices/counter/counterSlice.js
@@ -4,7 +4,8 @@ import { createSlice } from '@reduxjs/toolkit'
 export const counterSlice = createSlice({
     name: 'counter',
     initialState: {
-        counter: 10
+        counter: 10,
+        times: 0
     },
     reducers: {
         increment: (state) => {
@@ -13,9 +14,17 @@ export const counterSlice = createSlice({
             // which detects changes to a "draft state" and produces a brand new
             // immutable state based off those changes
             state.counter += 1;
+
         },
+        decrement: (state) => {
+            state.counter -=1;
+        },
+        incrementBy: (state, action) => {
+            console.log(action);
+            state.counter += action.payload;
+        }
     },
 })
 
 // Action creators are generated for each case reducer function
-export const { increment } = counterSlice.actions;
+export const { increment, decrement, incrementBy } = counterSlice.actions;
