b65c59d04b19f79b82d1f5e2b14b390a6412ad40
osmanjosue <osmanjosue@gmail.com>
2024-06-03 13:29:57 -0600

    Seccion 18 => Clase: pokemonSlice

---
+++ src/store/slices/pokemon/pokemonSlice.js
@@ -0,0 +1,24 @@
+import {
+    createSlice
+} from '@reduxjs/toolkit';
+
+export const pokemonSlice = createSlice({
+    name: 'pokemon',
+    initialState: {
+        page: 0,
+        pokemons: [],
+        isLoading: false,
+    },
+    reducers: {
+        startLoadingPokemons: (state) => {
+            state.isLoading = true;
+        },
+        setPokemons: (state, action) => {
+            console.log(action);
+        }
+    }
+});
+
+
+// Action creators are generated for each case reducer function
+export const { startLoadingPokemons, setPokemons } = pokemonSlice.actions;
