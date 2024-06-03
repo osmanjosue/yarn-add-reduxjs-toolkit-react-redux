6ce267698d896d6d41e318f596135d8fdc9c31eb
osmanjosue <osmanjosue@gmail.com>
2024-06-03 14:33:14 -0600

    Seccion 18 => Parte#1 => Clase: Axios

--- src/store/slices/pokemon/thunks.js
+++ src/store/slices/pokemon/thunks.js
@@ -1,11 +1,13 @@
-import { startLoadingPokemons } from "./pokemonSlice"
+import { setPokemons, startLoadingPokemons } from "./pokemonSlice"
 
 export const getPokemons = (page = 0) => {
-    return (dispatch, getState) => {
+    return async (dispatch, getState) => {
         dispatch(startLoadingPokemons());
+
+        const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10}`);
+        const data = await resp.json();
+        console.log(data);
+
+        dispatch(setPokemons({ pokemons: data.results, page: page + 1 }));
     }
-
-    //TODO: realizar petición http
-
-    //dispatch(setPokemons());
 }
