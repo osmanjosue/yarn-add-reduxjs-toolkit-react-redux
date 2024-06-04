f24a9834c5a6d0ea32e1a97764487b0efce8c3c5
osmanjosue <osmanjosue@gmail.com>
2024-06-03 20:43:43 -0600

    Seccion 18 => Clase: Mostrar los pokemons paginadamente

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { increment, decrement, incrementBy } from './store/slices/counter/counterSlice';
